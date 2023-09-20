565ffba8f271c540439a392f2687b87a1eb958b0
Alex Chukrii <chukrii.alex@gmail.com>
2023-09-20 17:16:24 +0200

    Added Router Library

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,5 @@
+function Dropdown() {
+  return <h1 className="text-3xl font-bold underline">Dropdown</h1>;
+}
+
+export default Dropdown;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dropdown from "./components/Dropdown/Dropdown";
 
+const router = createBrowserRouter([
+  {
+    path: "/dropdown",
+    element: <Dropdown />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
